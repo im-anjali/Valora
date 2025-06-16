f97a314cb0f68a48250709ac8d67c773af3fb288
Anjali Phule <anjaliphule5@gmail.com>
2025-06-16 11:47:43 +0530

    local change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
                     href={item.href}
                     aria-current={item.current ? 'page' : undefined}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-black ',
+                      item.current ? 'text' : 'text-black ',
                       'rounded-md px-3 py-2 text-sm font-medium',
                     )}
                   >
